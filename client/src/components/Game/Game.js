b61c5e8427bea0b35ceaa295bb13c4893bdade91
Eric Han <eric_h94@hotmail.com>
2020-07-12 16:29:55 -0400

    test

--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-<<<<<<< Updated upstream
 import FightPrototype from './FightPrototype';
 import CutscenePrototype from './CutscenePrototype';
 import MinigamePrototype from './MinigamePrototype';
 import Customizer from './CustomizerPrototype';
 import EricTest from './EricTest';
 import { scenes } from '../../chapters/gameScenes';
-<<<<<<< HEAD
 import CombatTestEnvironment from './EricTest'
-=======
 import { useDispatch, useSelector } from 'react-redux';
 import { setScene } from '../../actions';
->>>>>>> f739ad7c5ac61f679253895a702db515786c28c1
-
-=======
 import Overworld from './Overworld';
 import MenuBar from './MenuBar';
 import CharacterDisplay from './CharacterDisplay';
 import PlayerSetup from './PlayerSetup';
 import StatusBars from './StatusBars';
 import CombatUi from './EricTest/CombatUi'
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
+
 function Game() {
   const dispatch = useDispatch();
   const scene = useSelector((state) => state.game.scene);
@@ -45,11 +37,7 @@ function Game() {
       case 'cutscene':
         return <CutscenePrototype scene={scene.name} />;
       case 'fight':
-<<<<<<< HEAD
         return <CombatTestEnvironment />;
-=======
-        return <EricTest />;
->>>>>>> f739ad7c5ac61f679253895a702db515786c28c1
       case 'minigame':
         return <MinigamePrototype />;
       case 'customizer':
@@ -57,8 +45,6 @@ function Game() {
     }
   };
   return (
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     <GameBody>
       {scenes[`scene_${scene}`] ? (
         renderScene(scenes[`scene_${scene}`])
@@ -83,23 +69,6 @@ const GameBody = styled.div`
     'next';
   grid-template-rows: 11fr 0.8fr;
 `;
-=======
-    <div>
-      <Overworld />
-      <CombatUi/>
-    </div> 
-  );
-}
-
-=======
-    <div>
-      <Overworld />
-      <CombatUi/>
-    </div> 
-  );
-}
-
->>>>>>> Stashed changes
 // const GameBody = styled.div`
 //   display: grid;
 //   grid-template-areas:
@@ -112,9 +81,5 @@ const GameBody = styled.div`
 //   grid-template-columns: 1fr 1fr 1fr 1fr;
 //   max-height: 90vh;
 // `;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 export default Game;

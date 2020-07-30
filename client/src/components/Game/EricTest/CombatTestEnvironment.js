c4b03e260fae17952493e80bef9d8e364778add4
Eric Han <eric_h94@hotmail.com>
2020-07-29 20:39:33 -0400

    please work

--- client/src/components/Game/EricTest/CombatTestEnvironment.js
+++ client/src/components/Game/EricTest/CombatTestEnvironment.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-<<<<<<< Updated upstream
 import { movementTimeout, sleep, possiblePaths, pathfinder } from '../../../Helpers/playerMoveHelper'
 import { attackRange } from '../../../Helpers/playerCombatHelper'
 
 //components
 import CombatUi from './CombatUi';
-=======
-import { movementTimeout, sleep, possiblePaths, pathfinder } from '../../../Helpers/helper'
->>>>>>> Stashed changes
 
 const CombatTestEnvironment = () => {
   const [PLAYER_POS, SET_PLAYER_POS] = React.useState({ x: 5, y: 1 });
@@ -50,11 +46,6 @@ const CombatTestEnvironment = () => {
     possiblePaths(actionPoints, SET_PLAYER_MOVES, PLAYER_POS, width, height, OBSTRUCTIONS)
   }
 
-<<<<<<< Updated upstream
-=======
-  possiblePaths(actionPoints, SET_PLAYER_MOVES, PLAYER_POS, width, height, OBSTRUCTIONS)
-
->>>>>>> Stashed changes
   const playerMove = (x, y) => { // 
     // const movementData = movementTimeout({x:x, y:y},PLAYER_POS) //total time, xTime, yTime, isTurn, movement x and y
     // const TotalDistance = PLAYER_MOVES.find(sq => sq.x === x && sq.y === y).distance;
@@ -64,11 +55,8 @@ const CombatTestEnvironment = () => {
     const playerPath = pathfinder({x:x, y:y}, PLAYER_MOVES)
     // setActionPoints(actionPoints - TotalDistance)
     SET_PLAYER_POS({x:x, y:y})
-<<<<<<< Updated upstream
     SET_PLAYER_MOVES([])
     SET_TURN('playerAction')
-=======
->>>>>>> Stashed changes
     // playerAnimation(playerPath)
   }
 
@@ -79,7 +67,6 @@ const CombatTestEnvironment = () => {
     });
   };
 
-<<<<<<< Updated upstream
   return (
     <>
       <CombatUi 
@@ -126,35 +113,6 @@ const CombatTestEnvironment = () => {
                       {sq.x}, {sq.y}
                     </PossibleBox>
                   );
-=======
-    return (
-      <Wrapper>
-        {mapGrid.map((row, idx) => {
-          return (
-            <div key={idx} style={{display:'flex'}}>
-              {row.map((sq) => {
-                if(sq.x === PLAYER_POS.x && sq.y === PLAYER_POS.y) {
-                  return (
-                    <Player />
-                  )
-                }  else if (OBSTRUCTIONS.find(obs => sq.x === obs.x && sq.y === obs.y)){
-                  return (
-                    <Box>{OBSTRUCTIONS.find(obs => sq.x === obs.x && sq.y === obs.y).obstacle}</Box>
-                  )
-                } else if (sq.x === enemyLocation.x && sq.y === enemyLocation.y) { 
-                  return (
-                    <Enemy>enemy</Enemy>
-                  )
-                } else if (PLAYER_MOVES.find(obs => sq.x === obs.x && sq.y === obs.y)) {
-                  return (
-                    <PossibleBox 
-                    key={Math.random() * 100000}
-                    onClick = {() => playerMove(sq.x, sq.y)}
-                    >
-                      {sq.x}, {sq.y}
-                    </PossibleBox>
-                  )
->>>>>>> Stashed changes
                 } else {
                   return (
                     <Box
@@ -170,19 +128,12 @@ const CombatTestEnvironment = () => {
             </div>
           );
         })}
-<<<<<<< Updated upstream
       </Wrapper>
     </>
   );
 }
 
 
-=======
-    </Wrapper>
-  )
-}
-
->>>>>>> Stashed changes
 
 const Wrapper = styled.div`
   grid-area: ui;

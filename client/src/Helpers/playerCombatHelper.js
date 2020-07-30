b4ca8af404c4255b6bd65894812cfa02a22ffe0a
Eric Han <eric_h94@hotmail.com>
2020-07-29 20:07:40 -0400

    todo and reformatting

---
+++ client/src/Helpers/playerCombatHelper.js
@@ -0,0 +1,51 @@
+//what I'm trying to do here is pass the range, effect, position, and damage of each skill in an area
+
+export const attackRange = (skill, PLAYER_POS, width, height, OBSTRUCTIONS) => { // i need attack to return it's range, and then 
+  let range = skill.range
+  if(skill.pathing === 'radial') { //checks skill pathing and calls pathing function
+    return radialRange(range, PLAYER_POS, width, height, OBSTRUCTIONS)
+  }
+}
+
+//rangefinding functions
+
+//radial rangefinding
+
+const radialRange = (range, PLAYER_POS, width, height, OBSTRUCTIONS) => { //maybe i can get this from a global state variable
+  let solvedSpaces = [PLAYER_POS];
+  let distanceCounter = 1;
+  console.log(OBSTRUCTIONS)
+  for(let i = range; i > 0; i -= 1) {
+    solvedSpaces.forEach((node) => {
+      const unsolvedSpaces = [
+        { x: node.x - 1, y: node.y, distance: distanceCounter },
+        { x: node.x + 1, y: node.y, distance: distanceCounter },
+        { y: node.y - 1, x: node.x, distance: distanceCounter },
+        { y: node.y + 1, x: node.x, distance: distanceCounter },
+      ];
+      unsolvedSpaces.forEach((space) => {
+        if (
+          space.x > 0 &&
+          space.y > 0 &&
+          space.x <= width &&
+          space.y <= height &&
+          !OBSTRUCTIONS.find((obj) => obj.x === space.x && obj.y === space.y)
+        ) {
+          if (i === 0) {
+            return;
+          } else {
+            if(solvedSpaces.find(obj => (obj.x === space.x && obj.y === space.y)) === undefined) {
+              solvedSpaces.push(space);
+            } else {
+              return
+            }
+          }
+        } else {
+          return
+        }
+      })
+    })
+    distanceCounter += 1;
+  }
+  return solvedSpaces
+}
--- client/src/components/Game/EricTest/CombatTestEnvironment.js
+++ client/src/components/Game/EricTest/CombatTestEnvironment.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { movementTimeout, sleep } from '../../../Helpers/helper';
+import { movementTimeout, sleep, possiblePaths, pathfinder } from '../../../Helpers/playerMoveHelper'
+import { attackRange } from '../../../Helpers/playerCombatHelper'
+
+//components
+import CombatUi from './CombatUi';
 
 const CombatTestEnvironment = () => {
   const [PLAYER_POS, SET_PLAYER_POS] = React.useState({ x: 5, y: 1 });
   const [PLAYER_MOVES, SET_PLAYER_MOVES] = React.useState([]);
-  const [TURN, SET_TURN] = React.useState('player');
+  const [TURN, SET_TURN] = React.useState('idle');
+  const [ATTACK_RADIUS, SET_ATTACK_RADIUS] = React.useState([]);
   const [OBSTRUCTIONS, SET_OBSTRUCTIONS] = React.useState([
     { x: 1, y: 1, obstacle: 'turn buckle' },
     { x: 10, y: 10, obstacle: 'turn buckle' },
     { x: 1, y: 10, obstacle: 'turn buckle' },
     { x: 10, y: 1, obstacle: 'turn buckle' },
   ]);
-
+  
+  
   //temporary character state//
   const [actionPoints, setActionPoints] = React.useState(4);
   const [playerHP, setPlayerHP] = React.useState(10);
   const [enemyHP, setEnemyHP] = React.useState(10);
   const [enemyLocation, setEnemyLocation] = React.useState({ x: 5, y: 10 });
-
+  const playerSkills = [
+    {name: 'slap', range: 1, pathing: 'radial'},
+    {name: 'slap', range: 1, pathing: 'radial'},
+    {name: 'slap', range: 1, pathing: 'radial'},
+    {name: 'slap', range: 1, pathing: 'radial'},
+  ]
   const width = 10;
   const height = 10;
   let mapGrid = [];
@@ -29,64 +40,25 @@ const CombatTestEnvironment = () => {
       mapGrid[y - 1].push({ y: y, x: x, obst: 0 });
     }
   }
+  console.log(ATTACK_RADIUS)
+  if(TURN === 'idle') {
+    SET_TURN('playerMove')
+    possiblePaths(actionPoints, SET_PLAYER_MOVES, PLAYER_POS, width, height, OBSTRUCTIONS)
+  }
 
-  const possiblePaths = () => {
-    SET_PLAYER_MOVES([]);
-    let distanceCounter = 1;
-    let moves = actionPoints;
-    const possibleArray = [PLAYER_POS];
-    for (let i = moves; i > 0; i -= 1) {
-      possibleArray.forEach((node) => {
-        const unsolvedSpaces = [
-          { x: node.x - 1, y: node.y, distance: distanceCounter },
-          { x: node.x + 1, y: node.y, distance: distanceCounter },
-          { y: node.y - 1, x: node.x, distance: distanceCounter },
-          { y: node.y + 1, x: node.x, distance: distanceCounter },
-        ];
-        unsolvedSpaces.forEach((move) => {
-          if (
-            move.x > 0 &&
-            move.y > 0 &&
-            move.x <= width &&
-            move.y <= height &&
-            !OBSTRUCTIONS.find((obj) => obj.x === move.x && obj.y === move.y)
-          ) {
-            if (i === 0) {
-              return;
-            } else {
-              if (
-                possibleArray.find(
-                  (obj) => obj.x === move.x && obj.y === move.y
-                ) === undefined
-              ) {
-                possibleArray.push(move);
-              } else {
-                return;
-              }
-            }
-          } else {
-            return;
-          }
-        });
-      });
-      distanceCounter += 1;
-    }
-    SET_PLAYER_MOVES(possibleArray);
-  };
-  // console.log(PLAYER_MOVES)
-  const playerMove = (x, y) => {
-    //
+  const playerMove = (x, y) => { // 
     // const movementData = movementTimeout({x:x, y:y},PLAYER_POS) //total time, xTime, yTime, isTurn, movement x and y
-    const TotalDistance = PLAYER_MOVES.find((sq) => sq.x === x && sq.y === y)
-      .distance;
+    // const TotalDistance = PLAYER_MOVES.find(sq => sq.x === x && sq.y === y).distance;
     // console.log(x, y, 'xy')
     // console.log(actionPoints, 'action points')
     // console.log(TotalDistance, 'distance')
-    const playerPath = path({ x: x, y: y });
+    const playerPath = pathfinder({x:x, y:y}, PLAYER_MOVES)
     // setActionPoints(actionPoints - TotalDistance)
-    // SET_PLAYER_POS({x:x, y:y})
-    playerAnimation(playerPath);
-  };
+    SET_PLAYER_POS({x:x, y:y})
+    SET_PLAYER_MOVES([])
+    SET_TURN('playerAction')
+    // playerAnimation(playerPath)
+  }
 
   const playerAnimation = (path) => {
     path.forEach(async (move) => {
@@ -95,55 +67,29 @@ const CombatTestEnvironment = () => {
     });
   };
 
-  const path = (target) => {
-    // takes target location and extrapolates a path backwards
-    let endpoint = PLAYER_MOVES.find(
-      (obj) => obj.x === target.x && obj.y === target.y
-    );
-    console.log(endpoint, 'endpoint');
-    let previousSquare = endpoint;
-    const pathArray = [endpoint];
-    for (let i = endpoint.distance - 1; i > 0; i -= 1) {
-      const tempPath = PLAYER_MOVES.find(
-        (obj) =>
-          (obj.distance === i &&
-            obj.x === previousSquare.x - 1 &&
-            obj.y === previousSquare.y) ||
-          (obj.distance === i &&
-            obj.x === previousSquare.x + 1 &&
-            obj.y === previousSquare.y) ||
-          (obj.distance === i &&
-            obj.y === previousSquare.y + 1 &&
-            obj.y === previousSquare.x) ||
-          (obj.distance === i &&
-            obj.y === previousSquare.y - 1 &&
-            obj.x === previousSquare.x)
-      );
-      pathArray.push(tempPath);
-      previousSquare = tempPath;
-    }
-    return (
-      pathArray.sort((a, b) => {
-        return a.distance - b.distance;
-      }),
-      [PLAYER_POS, enemyLocation]
-    );
-  };
-
-  if (TURN === 'player') {
-    return (
+  return (
+    <>
+      <CombatUi 
+        turn={TURN} 
+        SET_ATTACK_RADIUS={SET_ATTACK_RADIUS} 
+        playerSkills={playerSkills}
+        PLAYER_POS={PLAYER_POS}
+        width={width}
+        height={height}
+        OBSTRUCTIONS={OBSTRUCTIONS}
+      />
       <Wrapper>
         {mapGrid.map((row, idx) => {
           return (
-            <div key={idx} style={{ display: 'flex' }}>
+            <div key={Math.random() * 100000} style={{ display: 'flex' }}>
               {row.map((sq) => {
                 if (sq.x === PLAYER_POS.x && sq.y === PLAYER_POS.y) {
-                  return <Player />;
+                  return <Player key={Math.random() * 100000} />;
                 } else if (
                   OBSTRUCTIONS.find((obs) => sq.x === obs.x && sq.y === obs.y)
                 ) {
                   return (
-                    <Box>
+                    <Box key={Math.random() * 100000}>
                       {
                         OBSTRUCTIONS.find(
                           (obs) => sq.x === obs.x && sq.y === obs.y
@@ -155,7 +101,7 @@ const CombatTestEnvironment = () => {
                   sq.x === enemyLocation.x &&
                   sq.y === enemyLocation.y
                 ) {
-                  return <Enemy>enemy</Enemy>;
+                  return <Enemy key={Math.random() * 100000}>enemy</Enemy>;
                 } else if (
                   PLAYER_MOVES.find((obs) => sq.x === obs.x && sq.y === obs.y)
                 ) {
@@ -182,56 +128,16 @@ const CombatTestEnvironment = () => {
           );
         })}
       </Wrapper>
-    );
-  } else {
-    return (
-      <Wrapper>
-        {mapGrid.map((row, idx) => {
-          return (
-            <div key={idx} style={{ display: 'flex' }}>
-              {row.map((sq) => {
-                if (sq.x === PLAYER_POS.x && sq.y === PLAYER_POS.y) {
-                  return <Player />;
-                } else if (
-                  OBSTRUCTIONS.find((obs) => sq.x === obs.x && sq.y === obs.y)
-                ) {
-                  return (
-                    <Box>
-                      {
-                        OBSTRUCTIONS.find(
-                          (obs) => sq.x === obs.x && sq.y === obs.y
-                        ).obstacle
-                      }
-                    </Box>
-                  );
-                } else if (
-                  sq.x === enemyLocation.x &&
-                  sq.y === enemyLocation.y
-                ) {
-                  return <Enemy>enemy</Enemy>;
-                } else {
-                  return (
-                    <Box
-                      key={Math.random() * 100000}
-                      style={{ border: '1px solid black' }}
-                    >
-                      {sq.x},{sq.y}
-                    </Box>
-                  );
-                }
-              })}
-            </div>
-          );
-        })}
-      </Wrapper>
-    );
-  }
-};
+    </>
+  );
+}
+
+
 
 const Wrapper = styled.div`
   grid-area: ui;
   width: auto;
-  height: auto;
+  height: 1000px;
   margin-top: 5%;
   margin-left: 5%;
 `;
@@ -277,4 +183,4 @@ const Path = styled.div`
   opacity: 0.8;
 `;
 
-export default CombatTestEnvironment;
+export default CombatTestEnvironment

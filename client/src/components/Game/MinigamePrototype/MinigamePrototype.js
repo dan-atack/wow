9ef428d4f8bc9d07948a641a21bbfe5ed9a338ee
dan-atack <dan_atack@hotmail.com>
2020-09-09 22:15:44 -0400

    skeletal minigame established

--- client/src/components/Game/MinigamePrototype/MinigamePrototype.js
+++ client/src/components/Game/MinigamePrototype/MinigamePrototype.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useTime } from '../../../hooks/useTime';
-import { useDispatch, useSelector } from 'react-redux';
-import { setKarma } from '../../../actions';
+import { useSelector } from 'react-redux';
+import MinigameButton from './MinigameButton';
 
 function MinigamePrototype() {
+  const karma = useSelector((state) => state.player.karma);
   // Trigger a re-render every 0.2 seconds:
   const now = useTime(200);
+  // Tick off time:
   const [ticker, setTicker] = React.useState(0);
+  // Button's value will be set initially:
+  const [buttonData, setButtonData] = React.useState({
+    alignment: 'good',
+    text: 'Something witty!',
+    karmicValue: 15,
+  });
   React.useEffect(() => {
     setTicker(ticker + 1);
+    // this is a hard-coded basic example of a value being modified by the timer:
+    if (ticker > 15 && karma === 0) {
+      setButtonData({
+        alignment: 'evil',
+        text: 'Something less witty.',
+        karmicValue: -10,
+      });
+    }
   }, [now]);
   return (
     <Wrapper>
       <h1>MINIGAME</h1>
-      <h1>{ticker}</h1>
+      <Karmometer karma={karma}>Player Karma: {karma}</Karmometer>
+      <MinigameButton buttonData={buttonData}></MinigameButton>
     </Wrapper>
   );
 }
@@ -23,6 +40,15 @@ const Wrapper = styled.div`
   grid-area: ui;
   margin: 4% 9%;
   border: 2px solid black;
+`;
+
+const Karmometer = styled.h2`
+  height: 128px;
+  width: 256px;
+  font-size: 2em;
+  background-color: ${(props) => (props.karma > 0 ? 'limegreen' : 'red')};
+  border: 3px solid whitesmoke;
+  border-radius: 18px;
 `;
 
 export default MinigamePrototype;

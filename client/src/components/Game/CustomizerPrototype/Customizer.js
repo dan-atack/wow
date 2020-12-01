d930175ac0c0f8f0839f9a75d2374faaf63b223d
dan-atack <dan_atack@hotmail.com>
2020-11-30 20:15:06 -0500

    Completed basic reflex check component with success/fail stoppers and indicators

--- client/src/components/Game/CustomizerPrototype/Customizer.js
+++ client/src/components/Game/CustomizerPrototype/Customizer.js
@@ -5,16 +5,16 @@ import ReflexCheck from '../ReflexCheck';
 import moveCombos from '../../../data/playerMoves.json';
 
 function Customizer({ words }) {
-  const randMove = Math.floor(Math.random() * 3);
+  const randMove = Math.floor(Math.random() * moveCombos.length);
   // It is easier if the randomization all occurs in the component that doesn't rerender all the time:
   const randCombo = Math.floor(Math.random() * 3);
+  const previousMoves = 3;// to simulate time-reduction for a move that is at the end of a chain of moves
   const move = moveCombos[randMove];
   return (
     <Wrapper>
       <h1>Character Customization and Sandbox UI!!!</h1>
       <h1>{words}</h1>
-      <ReflexCheck move={move} combo={randCombo} numPrevMoves={2}/>
-      {/* <h1>Skill points</h1> */}
+      <ReflexCheck move={move} combo={randCombo} numPrevMoves={previousMoves}/>
     </Wrapper>
   );
 }
